f89dae4257b1b07b06052f7e378cd52772edc33e
MOGESH A <mogesh1766@gmail.com>
2026-07-07 18:37:18 +0530

    Refactor main.tsx to use ReactDOM and register service worker

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/service-worker.js");
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>
 );
